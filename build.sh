028a14db381dc37b3e4bc34048f8c828446cd47a
tammycapa <tammara.capa@gmail.com>
2024-08-08 20:26:36 +1000

    Update build script

--- build.sh
+++ build.sh
@@ -1,24 +1,29 @@
+# Ensure Node.js and npm are available
+export PATH=$PATH:/usr/local/bin/
+
 # Delete executable if existing in order to prevent test from duplicating
 rm -rf resume_builder
 
 # Run unit tests and save results in a text file
-echo "\n========== Running unit tests... ==========\n"
+echo -e "\n========== Running unit tests... ==========\n"
 npx jest _tests_/main.test.js --coverage
 
 # Check if tests passed
 if [ $? -eq 0 ]; then
-  echo "\nUnit test complete. ALL tests PASSED."
+  echo -e "\nUnit test complete. ALL tests PASSED."
+    # Package application
+  echo -e "\n========== Packaging application... ==========\n"
+
+  npx electron-packager . resume-builder \
+    --platform=darwin \
+    --arch=x64 \
+    --out=resume_builder \
+    --overwrite
+
+  echo -e "\Build SUCCESSFUL\n"
+  echo -e "\nExecutable file 'resume-builder.app' successfully created!\n"
 else
-  echo "\nUnit test complete. SOME tests FAILED."
+  echo -e "\nUnit test complete. SOME tests FAILED.\n"
+  echo -e "\nBuild FAILED.\n"
+  exit 1
 fi
-
-# Package application
-echo "\n========== Packaging application... ==========\n"
-
-npx electron-packager . resume-builder \
-  --platform=darwin \
-  --arch=x64 \
-  --out=resume_builder \
-  --overwrite
-
-echo "\nExecutable file 'resume-builder.app' successfully created!\n"
